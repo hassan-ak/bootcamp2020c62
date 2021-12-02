522ca15019fb9a18b58614c5dd3527bf1c6045a1
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-12-02 20:18:59 +0500

    cdk app

---
+++ step14_CI_CD_pipeline/step03_CI_CD_pipeline_update_frontend/cdkBackend/test/cdk_backend.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as CdkBackend from '../lib/cdk_backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkBackend.CdkBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
