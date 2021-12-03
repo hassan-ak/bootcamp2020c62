b31c8a8e2aebdd13beab7592d48e0eccdb52126f
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-12-04 02:10:02 +0500

    Update index.tsx

--- step14_CI_CD_pipeline/step03_CI_CD_pipeline_update_frontend/gatsbyfrontend/src/pages/index.tsx
+++ step14_CI_CD_pipeline/step03_CI_CD_pipeline_update_frontend/gatsbyfrontend/src/pages/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div>
       <h1>Home Page for CI/CD Deployment</h1>
       <p>Update for CI/CD testing</p>
+      <p>Updated again for CI/CD testing</p>
     </div>
   );
 }
