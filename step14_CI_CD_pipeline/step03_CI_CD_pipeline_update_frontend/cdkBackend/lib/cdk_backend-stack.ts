0d3a26ce8d9bd60d544176c9badd9e2e2bb96aac
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-12-03 20:31:56 +0500

    create and deploy s3 bucket

--- step14_CI_CD_pipeline/step03_CI_CD_pipeline_update_frontend/cdkBackend/lib/cdk_backend-stack.ts
+++ step14_CI_CD_pipeline/step03_CI_CD_pipeline_update_frontend/cdkBackend/lib/cdk_backend-stack.ts
@@ -1,15 +1,29 @@
-import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origions from "@aws-cdk/aws-cloudfront-origins";
+import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
 
 export class CdkBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Deploy Gatsby on S3 bucket
+    // create new bucket
+    const myBucket = new s3.Bucket(this, "GatsbyBucket", {
+      bucketName: "GatsbyBucket",
+      versioned: true,
+      websiteIndexDocument: "index.html",
+    });
+    // create Cloudfront distribution
+    const dist = new cloudfront.Distribution(this, "GatsbyDistribution", {
+      defaultBehavior: { origin: new origions.S3Origin(myBucket) },
+    });
+    // create s3 deployment bundle
+    new s3Deployment.BucketDeployment(this, "DeployGatsbyWebsite", {
+      sources: [s3Deployment.Source.asset("../gatsbyfrontend/public")],
+      destinationBucket: myBucket,
+      distribution: dist,
+    });
   }
 }
